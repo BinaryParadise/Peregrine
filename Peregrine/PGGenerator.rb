d06d3ac8e1992401b8ef1e70529da85437ab71fe
Rake Yang <fenglaijun@gmail.com>
2019-12-18 22:02:12 +0800

    ruby shell merge

--- Peregrine/PGGenerator.rb
+++ Peregrine/PGGenerator.rb
@@ -2,9 +2,19 @@ require 'xcodeproj'
 require 'yaml'
 require 'json'
 
+BUILD_PHASE_NAME_FETCH_ENV = '[Peregrine] Generator Routing Table'
+
 class PGGenerator
 
   # 生成路由表
+  def initialize(args)
+    puts args.length
+    return
+    if args[0].class == 'xxxx'
+      generator(args)
+    end
+  end
+
   def self.generator(args)
     if args.length == 1
         args.push('-ferror-limit=0 -w')
@@ -15,7 +25,7 @@ class PGGenerator
     files = current_target.source_build_phase.files.to_a.map do |pbx_build_file|
         pbx_build_file.file_ref.real_path.to_s
     end
-    files = files.select {|file| File.extname(file).eql?(".swift") == false}
+    files = files.select {|file| File.extname(file).eql?(".m")}
     files = files.map do |file|
         "\""+file+"\""
     end
@@ -25,35 +35,96 @@ class PGGenerator
     if ENV['GCC_PRECOMPILE_PREFIX_HEADER'] == 'YES'
       include = '-include '+ENV['SRCROOT'] + '/' + ENV['GCC_PREFIX_HEADER']
     end
-#    -include #{ENV['SHARED_PRECOMPS_DIR']}/11336253693664976894/Header.pch.d \
+    framework_searchs = ENV['FRAMEWORK_SEARCH_PATHS'].split(' ')
 
     shell = "/usr/local/bin/clang-peregrine #{files.join(' ')} \
     -p=\"#{args[0]}\" \
     -- \
-    -fmodules -Werror -Wno-implicit-atomic-properties \
+    -fmodules -Wno-implicit-atomic-properties \
     -Wimplicit-function-declaration -fsyntax-only \
-    -fobjc-arc -ferror-limit=0 #{args[1]} \
+    -fobjc-arc #{args[1]} -fmacro-backtrace-limit=0 \
     -Wobjc-missing-super-calls \
     -isysroot #{ENV['SDKROOT']} \
     #{include} \
+    -I\"#{ENV['SRCROOT']}\" \
     -I#{header_searchs.join(' -I')} \
+    -F#{framework_searchs.join(' -F')} \
     -F#{ENV['DEVELOPER_FRAMEWORKS_DIR']} \
     "
-puts "#{shell}"
+    puts "#{shell}"
     `#{shell}`
-    # installer.analysis_result.targets.each do |target|
-    #   files = []
-    #   target.pod_targets.each do |pod_target|
-    #     pod_target.file_accessors.each do |file_accessor|
-    #       file_accessor.source_files.each do |file|
-    #         files.push(file.to_path)
-    #       end
-    #     end
-    #   end
-    #   puts files.class
-    # end
+  end
+
+  def self.configure_project(installer, pod_path=nil)
+    path = installer.sandbox.development_pods['Peregrine']
+    @dev_path = path ? path.dirname.to_s : nil
+
+    installer.analysis_result.targets.each do |target|
+      if target.user_project_path.exist? && target.user_target_uuids.any?
+        project = Xcodeproj::Project.open(target.user_project_path)
+        project_targets = self.project_targets(project, target)
+        self.add_shell_script(project_targets, project)
+      end
+
+    end
+
+    # 处理Pod的路由生成脚本
+    installer.pod_targets.each do |target|
+        if target.pod_name.index('NL') == 0
+          project = Xcodeproj::Project.open(installer.sandbox.root.to_s+"/"+target.pod_name+".xcodeproj")
+          self.add_shell_script(project.targets, project, pod_path)
+        end
+    end
+
+  end
+
+  def self.add_shell_script(project_targets, project, output = '${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}${WRAPPER_SUFFIX}')
+    install_targets = project_targets.select { |target| ['com.apple.product-type.application','com.apple.product-type.framework'].include?(target.product_type) }
+    install_targets.each do |project_target|
+      rubypath = (@dev_path == nil ?  "${PODS_ROOT}/Peregrine" : @dev_path) + "/Peregrine/PGGenerator.rb"
+
+      phase = self.fetch_exist_phase(BUILD_PHASE_NAME_FETCH_ENV, project_target)
+      if phase.nil?
+        phase = project_target.new_shell_script_build_phase(BUILD_PHASE_NAME_FETCH_ENV)
+      else
+        if false
+            phase.remove_from_project()
+        end
+      end
+
+      phase.run_only_for_deployment_postprocessing = "0"
+      phase.shell_script = "export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+ruby #{rubypath} \"#{output}/Peregrine.bundle\""
+
+      project.save()
+    end
+
+  end
+
+  def self.project_targets(project, target)
+    project_targets = Array.new
+
+    target.user_target_uuids.each do |user_target_uuid|
+      project.targets.each do |project_target|
+        if project_target.uuid.eql? user_target_uuid
+          project_targets.push(project_target)
+        end
+      end
+    end
+
+    return project_targets
+  end
+
+  #判断是否已存在
+  def self.fetch_exist_phase(phase_name, project_target)
+    project_target.build_phases.each do |build_phase|
+      next if build_phase.display_name != phase_name
+      #build_phase.remove_from_project
+      return build_phase
+    end
+    return nil
   end
 
 end
-
-PGGenerator::generator(ARGV)
