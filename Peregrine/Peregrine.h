234b2ec9f4c410ae5d1adafd3995826ebafa3434
Rake Yang <fenglaijun@gmail.com>
2020-05-13 10:58:20 +0800

    链接文档地址

--- Peregrine/Peregrine.h
+++ Peregrine/Peregrine.h
@@ -6,6 +6,8 @@
 //  Copyright © 2019 BinaryParadise. All rights reserved.
 //
 
+// 接入文档详见https://github.com/BinaryParadise/Peregrine
+
 #import <Peregrine/PGRouterContext.h>
 #import <Peregrine/PGRouterManager.h>
 #import <Peregrine/NSObject+Peregrine.h>
